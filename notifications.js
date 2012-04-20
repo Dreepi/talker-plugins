d3ed9ae9e71d02ca5e2d8e460e031aaf58bf4c5f
Esteban Pastorino <ejpastorino@gmail.com>
2012-04-20 20:45:30 -0300

    added html5 mention notification

---
+++ notifications.js
@@ -0,0 +1,30 @@
+if (!window.webkitNotifications) return;
+
+var getAvatar = function(user){
+  return "http://assets0.talkerapp.com/avatar/" + MD5(user.email) + ".jpg?s=48";
+};
+
+var currentUserRegexp = new RegExp("\\b"+Talker.currentUser.name+"\\b",'gi');
+
+plugin.onLoaded = function() {
+  plugin.onBlur = function() {
+    plugin.onMessageReceived = function(event) {
+      if (window.webkitNotifications.checkPermission() == 0) { // allowed
+        if (currentUserRegexp.test(event.content)){
+          var user = event.user;
+          var n = window.webkitNotifications.createNotification(getAvatar(user), user.name || '', event.content || '');
+          n.show();
+          n.ondisplay = function(){
+            setTimeout(function(){n.cancel()}, 5000);
+          };
+        }
+      } else {
+        window.webkitNotifications.requestPermission();
+      }
+    };
+  };
+
+  plugin.onFocus = function(){
+    plugin.onMessageReceived = function(){};
+  };
+};
