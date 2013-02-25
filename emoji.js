449cb4277e47e525464a526749cb8c4c8ee61d49
Esteban Pastorino <ejpastorino@gmail.com>
2013-02-25 12:05:55 -0300

    update emoji images location

--- emoji.js
+++ emoji.js
@@ -4,7 +4,7 @@ plugin.onMessageInsertion = function (event) {
   return Talker.getLastInsertion().replace(/:\S+:/g, function (emoji) {
     var filename = (emoji == ':+1:' ? ':plus1:' : emoji).replace(/:/g, '');
 
-    return "<img class=\"emoji\" title=\"" + emoji + "\" alt=\"" + emoji + "\" height=\"20\" width=\"20\" align=\"absmiddle\" src=\"https://beta.teambox.com/assets/emojis/" + filename + ".png\">"
+    return "<img class=\"emoji\" title=\"" + emoji + "\" alt=\"" + emoji + "\" height=\"20\" width=\"20\" align=\"absmiddle\" src=\"http://www.emoji-cheat-sheet.com/graphics/emojis/" + filename + ".png\">"
   });
 
 };
